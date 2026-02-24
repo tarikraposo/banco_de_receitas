45bf10c4e65b856e9ff3f1caa7fd59af041426be
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-24 18:34:40 -0300

    [#5] Importa formulário na página de criação de receitas.

--- src/app/features/receitas/pages/receita-create/receita-create.ts
+++ src/app/features/receitas/pages/receita-create/receita-create.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ReceitaForm } from "../../components/receita-form/receita-form";
 
 @Component({
   selector: 'app-receita-create',
-  imports: [],
+  imports: [ReceitaForm],
   templateUrl: './receita-create.html',
   styleUrl: './receita-create.scss',
 })

e14a323ec5477782fb1752fe3c9194fbd9695eb9
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-02-24 15:51:06 -0300

    [#1] Criação de componente de formulário.

---
+++ src/app/features/receitas/components/receita-form/receita-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receita-form',
+  imports: [],
+  templateUrl: './receita-form.html',
+  styleUrl: './receita-form.scss',
+})
+export class ReceitaForm {
+
+}

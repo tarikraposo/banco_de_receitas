53020a910387b1defe05d56dff5ebeb8d618d6b3
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-04 17:36:00 -0300

    [#13] Implementa Header. Adiciona informações básicas ao header.

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })

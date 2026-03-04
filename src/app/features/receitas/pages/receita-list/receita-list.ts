e6171a9d845004d1c9d57c098caab86065981857
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-04 17:30:17 -0300

    [#13] Implementa classe de listagem de receitas.

--- src/app/features/receitas/pages/receita-list/receita-list.ts
+++ src/app/features/receitas/pages/receita-list/receita-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReceitasService } from '../../services/receitas.service';
 
 @Component({
   selector: 'app-receita-list',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './receita-list.scss',
 })
 export class ReceitaList {
+  private receitasService = inject(ReceitasService);
 
+  receitas = signal<any[]>([]);
+  loading = signal(true);
+
+  async ngOnInit() {
+    await this.carregarReceitas();
+  }
+
+  async carregarReceitas() {
+    try {
+      const data = await this.receitasService.listarReceitasCompletas();
+      this.receitas.set(data ?? []);
+    } catch (err) {
+      console.error('Erro ao carregar receitas', err);
+    } finally {
+      this.loading.set(false);
+    }
+  }
 }

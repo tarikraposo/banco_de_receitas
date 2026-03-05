d87afc0d432635c0e8c780ad277002dc1be06365
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-05 14:06:15 -0300

    [#15] Adiciona routerLink. Adiciona novo botão.

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })

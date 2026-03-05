c0938b55ded1d58022045e40e2cf091d5f184565
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-05 11:24:38 -0300

    [#15] Implementa componente de card.

--- src/app/features/receitas/components/receita-card/receita-card.ts
+++ src/app/features/receitas/components/receita-card/receita-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReceitaInterface } from '../../../../core/models/receita-interface';
 
 @Component({
   selector: 'app-receita-card',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './receita-card.html',
   styleUrl: './receita-card.scss',
 })
 export class ReceitaCard {
-
+  receita = input.required<ReceitaInterface>();
+  receitaValue = computed(() => this.receita());
 }

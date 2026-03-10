e8cafa0c5d92f98aaf5d6926bccba78f5f9539ef
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-10 17:19:19 -0300

    [#19]feat: implementa componente  de detalhe de receitas.

--- src/app/features/receitas/pages/receita-detalhe/receita-detalhe.ts
+++ src/app/features/receitas/pages/receita-detalhe/receita-detalhe.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SupabaseClientService } from '../../../../core/supabase.client';
+import { ReceitaInterface } from '../../../../core/models/receita-interface';
+import { MatCardContent, MatCardTitle, MatCardSubtitle, MatCard, MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-receita-detalhe',
-  imports: [],
+  standalone: true,
   templateUrl: './receita-detalhe.html',
-  styleUrl: './receita-detalhe.scss',
+  imports:  [MatCardModule],
 })
-export class ReceitaDetalhe {
+export class ReceitaDetalheComponent implements OnInit {
 
+  private route = inject(ActivatedRoute);
+  private supabase = inject(SupabaseClientService).client;
+
+  receita = signal<ReceitaInterface | null>(null);
+  loading = signal(true);
+
+  async ngOnInit() {
+
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) return;
+
+    const { data, error } = await this.supabase
+      .from('receitas')
+      .select(`
+        *,
+        tipos(nome),
+        dificuldades(nome),
+        receita_ingredientes(
+          quantidade,
+          unidade,
+          ingredientes(nome)
+        )
+      `)
+      .eq('id', id)
+      .single();
+
+    if (error) {
+      console.error(error);
+    } else {
+      this.receita.set(data);
+    }
+
+    this.loading.set(false);
+  }
 }

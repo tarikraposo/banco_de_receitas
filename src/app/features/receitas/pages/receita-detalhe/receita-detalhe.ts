c5b082ef2e72208a0bc6a0bba259001b0bcd049c
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-10 17:39:30 -0300

    [#19]fix: altera query passando as fk corretas;

--- src/app/features/receitas/pages/receita-detalhe/receita-detalhe.ts
+++ src/app/features/receitas/pages/receita-detalhe/receita-detalhe.ts
@@ -2,16 +2,18 @@ import { Component, inject, signal, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SupabaseClientService } from '../../../../core/supabase.client';
 import { ReceitaInterface } from '../../../../core/models/receita-interface';
-import { MatCardContent, MatCardTitle, MatCardSubtitle, MatCard, MatCardModule } from "@angular/material/card";
+import {
+
+  MatCardModule,
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-receita-detalhe',
   standalone: true,
   templateUrl: './receita-detalhe.html',
-  imports:  [MatCardModule],
+  imports: [MatCardModule],
 })
 export class ReceitaDetalheComponent implements OnInit {
-
   private route = inject(ActivatedRoute);
   private supabase = inject(SupabaseClientService).client;
 
@@ -19,23 +21,24 @@ export class ReceitaDetalheComponent implements OnInit {
   loading = signal(true);
 
   async ngOnInit() {
-
     const id = this.route.snapshot.paramMap.get('id');
 
     if (!id) return;
 
     const { data, error } = await this.supabase
       .from('receitas')
-      .select(`
-        *,
-        tipos(nome),
-        dificuldades(nome),
-        receita_ingredientes(
-          quantidade,
-          unidade,
-          ingredientes(nome)
-        )
-      `)
+      .select(
+        `
+    *,
+    tipos!receitas_tipo_id_fkey(nome),
+    dificuldades!receitas_dificuldade_id_fkey(nome),
+    receita_ingredientes(
+      quantidade,
+      unidade,
+      ingredientes(nome)
+    )
+  `,
+      )
       .eq('id', id)
       .single();
 
